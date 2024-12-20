40f02dbf71fbf3993364e90e5b78414c129bd67e
Frank Lehner <frank-lehner71@t-online.de>
2024-12-20 08:08:34 +0100

    Style: Refactor docstrings

--- src/config.py
+++ src/config.py
@@ -1,13 +1,11 @@
-"""Configuration
-"""
+"""Configuration."""
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    """Class with configuration for Flask app
-    """
+    """Class with configuration for Flask app."""
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
